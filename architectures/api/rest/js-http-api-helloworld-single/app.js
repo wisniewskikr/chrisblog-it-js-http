62ec35d33a8604039c900a359ae9387ee8c78a53
wisniewskikr <wisniewskikr@gmail.com>
2023-10-18 12:52:53 +0200

    Init project js-http-api-helloworld-single

---
+++ architectures/api/rest/js-http-api-helloworld-single/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, function(error) {
+  if (error) {
+    console.log('Something went wrong', error)
+  } else {
+    console.log('Server is listening on port ' + port)
+  }
+});
