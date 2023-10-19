5d6cf6329723f71f9f881fe0c356a806d147996b
wisniewskikr <wisniewskikr@gmail.com>
2023-10-19 18:13:55 +0200

    Implements update

--- architectures/api/rest/js-http-api-rest-helloworld-crud/app.js
+++ architectures/api/rest/js-http-api-rest-helloworld-crud/app.js
@@ -8,9 +8,9 @@ class Message {
   }
 }
 
-class Error {
-  constructor( text ) {
-    this.text = text
+class Info {
+  constructor( info ) {
+    this.info = info
   }
 }
 
@@ -27,8 +27,12 @@ const server = http.createServer((req, res) => {
     handleReadAll(res);
   } else if ('get' == method) {
     handleRead(req, res);
+  } else if ('post' == method) {
+    handleCreate(req, res); 
+  } else if ('put' == method) {
+    handleUpdate(req, res);  
   } else {
-    displayMessage(new Error('Error: Resource Not Found'), res, 404);
+    displayMessage(new Info('Error: Resource Not Found'), res, 404);
   }
 
 });
@@ -57,9 +61,38 @@ function handleRead(req, res) {
   const id = req.url.split("/")[1];
   const messageId = parseInt(id);
   if (isNaN(messageId)) {
-    displayMessage(new Error('Specific ID Not Found'), res, 200);
+    displayMessage(new Info('Specific ID Not Found'), res, 200);
     return;
   }
   const message = messages.find((c) => c.id === messageId)
   displayMessage(message, res, 200);
+}
+
+function handleCreate(req, res) {
+  let body = '';
+  req.on('data', (chunk) => {
+      body += chunk;
+  });
+  req.on('end', () => {
+      messages.push(JSON.parse(body));
+      displayMessage(new Info('New Message was added'), res, 200);      
+  });
+}
+
+function handleUpdate(req, res) {
+  let body = '';
+  req.on('data', (chunk) => {
+      body += chunk;
+  });
+  req.on('end', () => {
+    const message = JSON.parse(body);
+    const messageCurrent = messages.find((c) => c.id === message.id)
+    if (!messageCurrent) {
+      displayMessage(new Info('Specific Message Not Found'), res, 200);
+      return; 
+    }
+    messages = messages.filter((c) => c.id !== message.id);
+    messages.push(message);
+    displayMessage(new Info('New Message was updated'), res, 200);      
+  });
 }
